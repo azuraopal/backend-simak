1dd4e9ef9c66d029b03eab6caf35ecf0a3c010fc
azuraopal <muhammadnovals334@gmail.com>
2025-01-08 23:13:54 +0700

    feat(Models): add Observer to detect changes in Upah

--- app/Models/Barang.php
+++ app/Models/Barang.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Barang extends Model
 {
@@ -20,5 +21,40 @@ class Barang extends Model
     public function barangHarian()
     {
         return $this->hasMany(BarangHarian::class, 'barang_id');
+    }
+
+    protected static function booted()
+    {
+        static::updated(function ($barang) {
+            if ($barang->isDirty('upah')) {
+                $barangHarians = DB::table('barang_harian')
+                    ->where('barang_id', $barang->id)
+                    ->get();
+
+                foreach ($barangHarians as $bh) {
+                    $upah = Upah::where('karyawan_id', $bh->karyawan_id)
+                        ->where('periode_mulai', '<=', $bh->tanggal)
+                        ->where('periode_selesai', '>=', $bh->tanggal)
+                        ->first();
+
+                    if ($upah) {
+                        $totalBH = DB::table('barang_harian as bh')
+                            ->join('barang as b', 'b.id', '=', 'bh.barang_id')
+                            ->where('bh.karyawan_id', $upah->karyawan_id)
+                            ->whereBetween('bh.tanggal', [$upah->periode_mulai, $upah->periode_selesai])
+                            ->select(
+                                DB::raw('COALESCE(SUM(bh.jumlah_dikerjakan), 0) as total_dikerjakan'),
+                                DB::raw('COALESCE(SUM(bh.jumlah_dikerjakan * b.upah), 0) as total_upah')
+                            )
+                            ->first();
+
+                        $upah->update([
+                            'total_dikerjakan' => $totalBH->total_dikerjakan,
+                            'total_upah' => $totalBH->total_upah
+                        ]);
+                    }
+                }
+            }
+        });
     }
 }

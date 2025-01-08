8a7a160f7af961b970915dc6b66c92c88922d02e
azuraopal <muhammadnovals334@gmail.com>
2025-01-08 23:10:57 +0700

    perf(Models): add function DetailPerhitungan and recalculateTotal for Upah

--- app/Models/Upah.php
+++ app/Models/Upah.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Upah extends Model
 {
@@ -32,14 +33,32 @@ class Upah extends Model
 
     public function detailPerhitungan()
     {
-        return $this->hasManyThrough(
-            BarangHarian::class,
-            Karyawan::class,
-            'id',
-            'barang_id',
-            'karyawan_id',
-            'id'
-        )->join('barang', 'barang.id', '=', 'barang_harian.barang_id')
-            ->select('barang_harian.*', 'barang.nama as nama_barang', 'barang.upah as upah_per_kodi');
+        return $this->hasMany(BarangHarian::class, 'karyawan_id', 'karyawan_id')
+            ->join('barang as b', 'b.id', '=', 'barang_harian.barang_id')
+            ->select(
+                'barang_harian.*',
+                'b.nama as nama_barang',
+                'b.upah as upah_per_kodi'
+            );
+    }
+
+    public function recalculateTotal()
+    {
+        $result = DB::table('barang_harian as bh')
+            ->join('barang as b', 'b.id', '=', 'bh.barang_id')
+            ->where('bh.karyawan_id', $this->karyawan_id)
+            ->whereBetween('bh.tanggal', [$this->periode_mulai, $this->periode_selesai])
+            ->select(
+                DB::raw('COALESCE(SUM(bh.jumlah_dikerjakan), 0) as total_dikerjakan'),
+                DB::raw('COALESCE(SUM(bh.jumlah_dikerjakan * b.upah), 0) as total_upah')
+            )
+            ->first();
+
+        if ($result) {
+            $this->update([
+                'total_dikerjakan' => $result->total_dikerjakan,
+                'total_upah' => $result->total_upah
+            ]);
+        }
     }
 }

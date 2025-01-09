25606813403d6b9b091671be9c0ca451db180e28
azuraopal <muhammadnovals334@gmail.com>
2025-01-09 08:01:18 +0700

    feat(Seeder): add seeder for Karyawan

---
+++ database/seeders/KaryawanSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Karyawan;
+use Illuminate\Database\Seeder;
+
+class KaryawanSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Karyawan::create([ 
+            "users_id"=> 3,
+            "nama"=> "Milki Dwi",
+            "tanggal_lahir"=> "26-12-2024",
+            "pekerjaan"=> "Lem Atas",
+            "alamat"=> "Jl. Pintu",
+            "telepon"=> "08123456789"
+        ]);
+    }
+}

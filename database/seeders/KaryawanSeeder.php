a3303a6fc784789fc4f506e33837d623aae4e002
azuraopal <muhammadnovals334@gmail.com>
2025-01-09 08:16:13 +0700

    fix(Seeder): fix error foreign key in karyawan

--- database/seeders/KaryawanSeeder.php
+++ database/seeders/KaryawanSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Karyawan;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class KaryawanSeeder extends Seeder
@@ -12,13 +13,15 @@ class KaryawanSeeder extends Seeder
      */
     public function run(): void
     {
+        $user = User::find(2);
         Karyawan::create([ 
             'users_id'=> 2,
             'nama'=> 'Milki Dwi',
             'tanggal_lahir'=> '26-12-2024',
             'pekerjaan'=> 'Lem Atas',
             'alamat'=> 'Jl. Pintu',
-            'telepon'=> '08123456789'
+            'telepon'=> '08123456789',
+            'email' => 'user->email'
         ]);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,12 +13,12 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::create([ 
-            'nama_lengkap' => 'Admin',
-            'email' => 'muhammadnovals334@gmail.com',
-            'password' => Hash::make('nopal123'),
-            'role' => 'Admin',
-        ]);
+        // User::create([ 
+        //     'nama_lengkap' => 'Admin',
+        //     'email' => 'muhammadnovals334@gmail.com',
+        //     'password' => Hash::make('nopal123'),
+        //     'role' => 'Admin',
+        // ]);
 
         User::create([ 
             'nama_lengkap' => 'Karyawan',

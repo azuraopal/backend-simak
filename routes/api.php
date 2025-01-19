7947db32b68bb3345f36de780948a4bd4c5c7d99
azuraopal <muhammadnovals334@gmail.com>
2025-01-19 19:47:15 +0700

    refactor(routes): update API routes for staff users

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\Admin\BarangController;
@@ -9,38 +8,24 @@ use App\Http\Controllers\Api\Admin\KaryawanController;
 use App\Http\Controllers\Api\Admin\KategoriController;
 use App\Http\Controllers\Api\Admin\BarangHarianController;
 
-
 // Auth Routes
 Route::post('/login', [AuthController::class, 'login']);
-
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::get('/profile', [AuthController::class, 'profile'])->middleware('auth:sanctum');
 
 // Public Routes
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/register/admin', [UserController::class, 'registerByAdmin']);
-    Route::post('/register/staff', [UserController::class, 'registerByStaff']);
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/profile', [AuthController::class, 'profile']);
     Route::post('/users/upload-photo', [UserController::class, 'uploadPhoto']);
     Route::post('/users/change-password', [UserController::class, 'changePassword']);
-    Route::get('/upah', [UpahController::class, 'index']);
-    Route::get('/upah/{id}', [UpahController::class, 'show']);
-    Route::get('upah/week/{weekNumber}', [UpahController::class, 'getByWeek']);
-    Route::get('/kategori', [KategoriController::class, 'index']);
-    Route::get('/kategori/{id}', [KategoriController::class, 'show']);
-    Route::get('/barang', [BarangController::class, 'index']);
-    Route::get('/barang/{id}', [BarangController::class, 'show']);
-    Route::get('/barang-harian', [BarangHarianController::class, 'index']);
-    Route::get('/barang-harian/{id}', [BarangHarianController::class, 'show']);
 });
 
-
 // Routes (Admin)
-Route::middleware(['auth:sanctum', 'admin'])->group(function () {
+Route::prefix('admin')->middleware(['auth:sanctum', 'admin'])->group(function () {
 
     // User Management
     Route::prefix('users')->group(function () {
-        Route::get('/', [UserController::class, 'index'])->name('users.index');
-        Route::post('/', [UserController::class, 'store'])->name('users.store');
+        Route::get('/', [UserController::class, 'index'])->name('admin.users.index');
+        Route::post('/', [UserController::class, 'store'])->name('admin.users.store');
         Route::get('/{id}', [UserController::class, 'show'])->name('users.show');
         Route::put('/{id}', [UserController::class, 'update'])->name('users.update');
         Route::delete('/{id}', [UserController::class, 'destroy'])->name('users.destroy');
@@ -58,6 +43,9 @@ Route::middleware(['auth:sanctum', 'admin'])->group(function () {
 
     // Upah Management
     Route::prefix('upah')->group(function () {
+        Route::get('/', [UpahController::class, 'index'])->name('upah.index');
+        Route::get('/{id}', [UpahController::class, 'show'])->name('upah.show');
+        Route::get('/week/{weekNumber}', [UpahController::class, 'getByWeek'])->name('upah.getByWeek');
         Route::post('/', [UpahController::class, 'store'])->name('upah.store');
         Route::put('/{id}', [UpahController::class, 'update'])->name('upah.update');
         Route::delete('/{id}', [UpahController::class, 'destroy'])->name('upah.destroy');
@@ -65,6 +53,8 @@ Route::middleware(['auth:sanctum', 'admin'])->group(function () {
 
     // Kategori Management
     Route::prefix('kategori')->group(function () {
+        Route::get('/', [KategoriController::class, 'index'])->name('kategori.index');
+        Route::get('/{id}', [KategoriController::class, 'show'])->name('kategori.show');
         Route::post('/', [KategoriController::class, 'store'])->name('kategori.store');
         Route::put('/{id}', [KategoriController::class, 'update'])->name('kategori.update');
         Route::delete('/{id}', [KategoriController::class, 'destroy'])->name('kategori.destroy');
@@ -72,6 +62,8 @@ Route::middleware(['auth:sanctum', 'admin'])->group(function () {
 
     // Barang Management
     Route::prefix('barang')->group(function () {
+        Route::get('/', [BarangController::class, 'index'])->name('barang.index');
+        Route::get('/{id}', [BarangController::class, 'show'])->name('barang.show');
         Route::post('/', [BarangController::class, 'store'])->name('barang.store');
         Route::put('/{id}', [BarangController::class, 'update'])->name('barang.update');
         Route::delete('/{id}', [BarangController::class, 'destroy'])->name('barang.destroy');
@@ -79,9 +71,43 @@ Route::middleware(['auth:sanctum', 'admin'])->group(function () {
 
     // Barang Harian Management
     Route::prefix('barang-harian')->group(function () {
+        Route::get('/', [BarangHarianController::class, 'index'])->name('barang-harian.index');
+        Route::get('/{id}', [BarangHarianController::class, 'show'])->name('barang-harian.show');
         Route::post('/', [BarangHarianController::class, 'store'])->name('barang-harian.store');
         Route::put('/{id}', [BarangHarianController::class, 'update'])->name('barang-harian.update');
         Route::delete('/{id}', [BarangHarianController::class, 'destroy'])->name('barang-harian.destroy');
     });
+});
 
+// Routes (Staff)
+Route::prefix('staff')->middleware(['auth:sanctum', 'staff'])->group(function () {
+
+    Route::prefix('users')->group(function () {
+        Route::post('/register/karyawan', [UserController::class, 'store'])
+            ->name('staff.users.registerKaryawan');
+    });
+
+    // Kategori Management
+    Route::prefix('kategori')->group(function () {
+        Route::post('/', [KategoriController::class, 'store'])->name('staff.kategori.store');
+        Route::get('/', [KategoriController::class, 'index'])->name('staff.kategori.index');
+        Route::get('/{id}', [KategoriController::class, 'show'])->name('staff.kategori.show');
+    });
+
+    // Upah Management
+    Route::prefix('upah')->group(function () {
+        Route::post('/', [UpahController::class, 'store'])->name('staff.upah.store');
+    });
+
+    // Barang Management
+    Route::prefix('barang')->group(function () {
+        Route::post('/', [BarangController::class, 'store'])->name('staff.barang.store');
+        Route::get('/', [BarangController::class, 'index'])->name('staff.barang.index');
+        Route::get('/{id}', [BarangController::class, 'show'])->name('staff.barang.show');
+    });
+
+    // Stock Management
+    Route::prefix('stock')->group(function () {
+        Route::post('/{id}', [BarangController::class, 'addStock'])->name('staff.stock.add');
+    });
 });

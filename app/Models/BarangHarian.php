ec0bf6006d0c191785a06714bb8d3d6bfae2be2c
azuraopal <muhammadnovals334@gmail.com>
2025-04-14 21:37:44 +0700

    Add status_pekerjaan field in BarangHarian model

--- app/Models/BarangHarian.php
+++ app/Models/BarangHarian.php
@@ -14,6 +14,7 @@ class BarangHarian extends Model
         'tanggal',
         'jumlah_dikerjakan',
         'status',
+        'status_pekerjaan',
         'tanggal_pengajuan',
         'tanggal_pengeluaran',
         'alasan_penolakan',

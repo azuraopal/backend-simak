b968e94f8562aa839b589abfb49aee8207f4b24c
azuraopal <muhammadnovals334@gmail.com>
2025-04-15 21:52:11 +0700

    Refactor(PengajuanBarangNotification): Update URL format based on user role

--- app/Notifications/PengajuanBarangNotification.php
+++ app/Notifications/PengajuanBarangNotification.php
@@ -2,6 +2,7 @@
 
 namespace App\Notifications;
 
+use App\Enums\UserRole;
 use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Notification;
 
@@ -23,13 +24,22 @@ class PengajuanBarangNotification extends Notification
 
     public function toDatabase($notifiable)
     {
+        $role = $notifiable->role;
+
+        $prefix = match ($role) {
+            UserRole::Admin => 'admin',
+            UserRole::StaffAdministrasi => 'staff-administrasi',
+            default => 'user',
+        };
+
         return [
             'barang_harian_id' => $this->barangHarian->id,
             'message' => 'Pengajuan barang baru dari ' . $this->barangHarian->staff_produksi->nama,
             'tanggal_pengajuan' => $this->barangHarian->tanggal_pengajuan,
-            'url' => url('/admin/pengajuan/' . $this->barangHarian->id),
+            'url' => url("/{$prefix}/pengajuan/" . $this->barangHarian->id),
         ];
     }
+
 
     public function toArray($notifiable)
     {
